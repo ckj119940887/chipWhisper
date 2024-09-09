fdf6e0d2bb8dee4887af5eee692b5a3439d10f3b
kejun <ckj119940887@gmail.com>
2024-09-09 15:32:54 -0500

    power_hamming_relationship

---
+++ generic_connect.py
@@ -0,0 +1,62 @@
+import chipwhisperer as cw
+import time
+
+try:
+    if not scope.connectStatus:
+        scope.con()
+except NameError:
+    scope = cw.scope()
+
+try:
+    if SS_VER == "SS_VER_2_1":
+        target_type = cw.targets.SimpleSerial2
+    elif SS_VER == "SS_VER_2_0":
+        raise OSError("SS_VER_2_0 is deprecated. Use SS_VER_2_1")
+    else:
+        target_type = cw.targets.SimpleSerial
+except:
+    SS_VER="SS_VER_1_1"
+    target_type = cw.targets.SimpleSerial
+
+try:
+    target = cw.target(scope, target_type)
+except:
+    print("INFO: Caught exception on reconnecting to target - attempting to reconnect to scope first.")
+    print("INFO: This is a work-around when USB has died without Python knowing. Ignore errors above this line.")
+    scope = cw.scope()
+    target = cw.target(scope, target_type)
+
+print("INFO: Found ChipWhisperer😍")
+
+if "STM" in PLATFORM or PLATFORM == "CWLITEARM" or PLATFORM == "CWNANO":
+    prog = cw.programmers.STM32FProgrammer
+elif PLATFORM == "CW303" or PLATFORM == "CWLITEXMEGA":
+    prog = cw.programmers.XMEGAProgrammer
+elif "neorv32" in PLATFORM.lower():
+    prog = cw.programmers.NEORV32Programmer
+elif PLATFORM == "CW308_SAM4S" or PLATFORM == "CWHUSKY":
+    prog = cw.programmers.SAM4SProgrammer
+else:
+    prog = None
+
+time.sleep(0.05)
+scope.default_setup()
+
+def reset_target(scope):
+    if PLATFORM == "CW303" or PLATFORM == "CWLITEXMEGA":
+        scope.io.pdic = 'low'
+        time.sleep(0.1)
+        scope.io.pdic = 'high_z' #XMEGA doesn't like pdic driven high
+        time.sleep(0.1) #xmega needs more startup time
+    elif "neorv32" in PLATFORM.lower():
+        raise IOError("Default iCE40 neorv32 build does not have external reset - reprogram device to reset")
+    elif PLATFORM == "CW308_SAM4S" or PLATFORM == "CWHUSKY":
+        scope.io.nrst = 'low'
+        time.sleep(0.25)
+        scope.io.nrst = 'high_z'
+        time.sleep(0.25)
+    else:
+        scope.io.nrst = 'low'
+        time.sleep(0.05)
+        scope.io.nrst = 'high_z'
+        time.sleep(0.05)
